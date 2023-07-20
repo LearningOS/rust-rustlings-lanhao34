1df630d766b6cdcc49da99fa90dead1a03461a93
lanhao34 <lanhao34@gmail.com>
2023-07-20 07:41:44 -0400

    fix threads1.rs

--- exercises/threads/threads1.rs
+++ exercises/threads/threads1.rs
@@ -18,7 +18,7 @@ fn main() {
     let mut completed_threads = 0;
     for handle in handles {
         // TODO: a struct is returned from thread::spawn, can you use it?
-        handle.join();
+        handle.join().unwrap();
         completed_threads += 1;
     }
 
